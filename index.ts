c5471639155e4004b5df8ac5e67d813abe5ebaf3
Rain <448130604@qq.com>
2022-12-09 16:40:40 +0800

    设置缓存及只接受get请求

--- index.ts
+++ index.ts
@@ -6,10 +6,18 @@ import * as url from 'url'
 
 const server = http.createServer()
 const publicDir = p.resolve(__dirname, 'public')  //找到public的相对路径
+let cacheAge = 3600 * 24 * 365
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   const {method, url: path, headers} = request
   const {pathname, search} = url.parse(path as string)
+
+  if (method !== 'GET') {
+    response.statusCode = 405
+    response.end()
+    return
+  }
+
   // response.setHeader('Content-Type', 'text/html;charset=utf-8')
   let filename = pathname!.substr(1)
   if (filename === '') {
@@ -30,6 +38,8 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
         response.end('服务器繁忙')
       }
     } else {
+      //返回文件内容
+      response.setHeader('Cache-Control', `public,max-age=${cacheAge}`)
       response.end(data)
     }
   })
